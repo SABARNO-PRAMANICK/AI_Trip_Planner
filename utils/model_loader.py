c038b15993de7e43c190f53b15dc2ea918e87826
sabsai <sabarnopramanick@gmail.com>
2025-08-08 20:58:52 +0530

    workflow and loader setup

---
+++ utils/model_loader.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from typing import Optional, Any, Literal
+from pydantic import BaseModel, field
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+class ConfigLoader:
+    def __init__(self):
+        print("Loading configuration...")
+        self.config=load_config()
+    
+    def __getitem__(self,key):
+        return self.config[key]
+
+class ModelLoader(BaseModel):
+    
+    model_provider: Literal["groq","openai"]= "groq"
+    config: Optional[ConfigLoader]=Field(default=None,exclude=True)
+
+    def model_post_init(self,__context: Any) -> None:
+        self.config=ConfigLoader()
+
+    class Config:
+        arbitrary_types_allowed=True
+    
+    def load_llm(self):
+        """
+        Load the LLM model based on the provider specified in the config.
+        """
+        print("Loading LLM model...")
+        print(f"Model provider: {self.model_provider}")
+        if self.model_provider=="groq":
+            print("Loading Groq model...")
+            groq_api_key = os.getenv("GROQ_API_KEY")
+            model_name= self.config["llm"]["groq"]["model_name"]
+            llm=ChatGroq(
+                model=model_name,
+                api_key=groq_api_key,
+            )
+        return llm
